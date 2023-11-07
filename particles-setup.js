1cab53df7b3a96f335ba7a359bfb964d7338174d
Jack Dolby <jack@w3forge.xyz>
2023-11-07 16:34:55 +0000

    Commit new site via Git for first time

---
+++ particles-setup.js
@@ -0,0 +1,45 @@
+particlesJS('particles-js', {
+    particles: {
+      number: {
+        value: 15, /* Number of particles */
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: '#ffffff' /* Particle color */
+      },
+      shape: {
+        type: 'circle',
+      },
+      opacity: {
+        value: 0.3, /* Particle opacity */
+        random: false
+      },
+      size: {
+        value: 1, /* Particle size */
+        random: true
+      },
+      line_linked: {
+        enable: true,
+        distance: 150, /* Distance to draw a line between two particles */
+        color: '#ffffff', /* Line color */
+        opacity: 0.4, /* Line opacity */
+        width: 1 /* Line width */
+      },
+      move: {
+        speed: 0.75 /* Particle movement speed */
+      }
+    },
+    interactivity: {
+      events: {
+        onhover: {
+          enable: true,
+          mode: 'grab' /* Line follows the cursor on hover */
+        }
+      }
+    },
+    retina_detect: true
+  });
+  
